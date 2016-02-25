6ef7e76e768a5853d936b7d836a591cec7deff19
hiroshi310 <hiro560428@gmail.com>
2016-02-25 02:41:44 +0000

    want have kadai

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -2,17 +2,20 @@ class OwnershipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    if params[:asin]
-      @item = Item.find_or_initialize_by(asin: params[:asin])
+    if params[:asin] #もしもアマゾンのデータだったら
+      @item = Item.find_or_initialize_by(asin: params[:asin]) #Itemテーブルを取得して無ければ新規作成
     else
       @item = Item.find(params[:item_id])
+      @item.save
     end
 
-    # itemsテーブルに存在しない場合はAmazonのデータを登録する。
+    # @itemがitemsテーブルに存在しない場合はAmazonのデータを登録する。
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = {}
+        response = Amazon::Ecs.item_lookup(params[:asin] ,  #アマゾンのデータ取得して
+                                  :response_group => 'Medium' , 
+                                  :country => 'jp')
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
@@ -24,22 +27,39 @@ class OwnershipsController < ApplicationController
       @item.large_image  = amazon_item.get("LargeImage/URL")
       @item.detail_page_url = amazon_item.get("DetailPageURL")
       @item.raw_info        = amazon_item.get_hash
-      @item.save!
+      @item.save! #Itemの中に情報を保存
     end
+    
+    # typeがhaveの場合、ownaershipテーブルに情報を保存したい type:haveとして保存、
+    if params[:type] == 'Have' #haveの場合は
+      current_user.have(@item) #current_userにhaveメソッドを実行する
+    # wantしている商品
+    else
+      current_user.want(@item)
+    end
+      
+  end
 
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値ににHaveボタンが押された時には「Have」,
     # Wantボタンがされた時には「Want」が設定されています。
     
-
-  end
-
   def destroy
     @item = Item.find(params[:item_id])
-
+    if params[:type] == 'Have' #haveの場合は
+      current_user.unhave(@item) #current_userにhaveメソッドを実行する
+    # wantしている商品
+    else
+      current_user.unwnat(@item)
+    end
+    
     # TODO 紐付けの解除。 
-    # params[:type]の値ににHavedボタンが押された時には「Have」,
-    # Wantedボタンがされた時には「Want」が設定されています。
+    # params[:type]の値にHave itボタンが押された時には「Have」,
+    # Want itボタンが押された時には「Want」が設定されています。
+   
+    #@item = current_user.haves.Item.find(params[:item_id]).unhave
+    #current_user.unhave(@item)
+    
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-
+  
   get    'signup', to: 'users#new'
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :ownerships do
+    member do
+      get 'have'
+      get 'want'
+    end
+  end
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :relationships, only: [:create, :destroy]
-  resources :ownerships, only: [:create, :destroy]
+  resources :ownerships, only: [:create, :destroy,]
   resources :items , only: [:new , :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
