2284a7fe25f950b836c969ffe310428f4edb0395
hiroshi310 <hiro560428@gmail.com>
2016-02-25 06:01:51 +0000

    item&userpage kadai

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,6 @@ Rails.application.routes.draw do
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :ownerships do
-    member do
-      get 'have'
-      get 'want'
-    end
-  end
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
